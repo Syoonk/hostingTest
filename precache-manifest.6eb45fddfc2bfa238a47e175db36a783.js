646f9cb02ff576d52d6c4e88d44c069a196c9f99
Sungyoon Kong <koseyo89@gmail.com>
2020-08-23 07:58:10 +0900

    Updates

---
+++ precache-manifest.6eb45fddfc2bfa238a47e175db36a783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8486cff44028656e5b240ac4eb6e26e0",
+    "url": "/deployingtest/index.html"
+  },
+  {
+    "revision": "4562a6a72260850d37a0",
+    "url": "/deployingtest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c00202ffad0d82b5b8d",
+    "url": "/deployingtest/static/js/2.0d893b1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployingtest/static/js/2.0d893b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4562a6a72260850d37a0",
+    "url": "/deployingtest/static/js/main.7a06bee7.chunk.js"
+  },
+  {
+    "revision": "70a2a049509414f7a2b0",
+    "url": "/deployingtest/static/js/runtime-main.aeab0428.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployingtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
